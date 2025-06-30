b09043841857e284b1ecff3e6b9403e3a23d796f
Shreyas Taware <shreyastaware6@gmail.com>
2025-06-30 23:09:59 +0530

    added origin to be netlify frontend thus resolving CORS issue

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # Allow CORS for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://mood-2-music.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
